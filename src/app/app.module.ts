099299f052e1c8403bf237709f1b04f396090fd3
Joosep Parts <joosep.parts@gmail.com>
2022-07-11 21:21:00 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgxEverythingUppercaseModule } from 'ngx-everything-uppercase';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxEverythingUppercaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
